22387e93ccab46b25de852692efaebde32bfdcc2
liar <2856570394@qq.com>
2024-03-07 12:26:39 +0800

    initial

---
+++ src/main/java/com/diffste/diffsteback/DiffsteBackApplication.java
@@ -0,0 +1,13 @@
+package com.diffste.diffsteback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiffsteBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiffsteBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diffste/diffsteback/DiffsteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diffste.diffsteback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiffsteBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
