9311047b01032bdc0735c0ac0ccae1d6b1036b8c
liar <2856570394@qq.com>
2024-03-08 12:14:32 +0800

    register added

--- src/main/java/com/diffste/diffsteback/DiffsteBackApplication.java
+++ src/main/java/com/diffste/diffsteback/DiffsteBackApplication.java
@@ -3,9 +3,9 @@ package com.diffste.diffsteback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 启动类
 @SpringBootApplication
 public class DiffsteBackApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DiffsteBackApplication.class, args);
     }
